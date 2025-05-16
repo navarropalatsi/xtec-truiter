9d91d5e63813f810d9a6a031cb5e3db340ec02ab
Joan Navarro Palatsi <joannavarropalatsi@gmail.com>
2025-05-16 08:10:14 +0200

    Inici projecte

---
+++ truiter/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for truiter project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'truiter.settings')
+
+application = get_asgi_application()
