0d33298dc8eefb32dbc46e8a4e4ef02d8f963378
Joan Navarro Palatsi <joannavarropalatsi@gmail.com>
2025-06-11 09:59:20 +0200

    Modificar perfil

--- xarxa/urls.py
+++ xarxa/urls.py
@@ -5,14 +5,21 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("", views.home, name="home"),
+    
     path("u/<str:username>/", views.profile, name="profile"),
+    path("u/<str:username>/update_profile/", views.update_profile_form, name="update_profile_form"),
+    path("u/<str:username>/update_profile/submit/", views.update_profile, name="update_profile"),
+    path("u/<str:username>/update_profile_picture/", views.update_profile_picture, name="update_profile_picture"),
+    
+    path("u/<str:username>/follow/", views.follow_user, name="follow_user"),
+    path("u/<str:username>/unfollow/", views.unfollow_user, name="unfollow_user"),
+
     path("p/new/", views.create_post, name="create_post"),
     path("p/<int:post_id>", views.post, name="post"),
     path("p/<int:post_id>/like/", views.like_post, name="like_post"),
     path("p/<int:post_id>/add_comment/", views.add_comment, name="add_comment"),
     path("p/<int:post_id>/repost_post/", views.repost_post, name="repost_post"),
-    path("u/<str:username>/follow/", views.follow_user, name="follow_user"),
-    path("u/<str:username>/unfollow/", views.unfollow_user, name="unfollow_user"),
+
     path("signup/", views.signup, name="signup"),
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- xarxa/views.py
+++ xarxa/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login
-
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
@@ -28,17 +28,63 @@ def home(request):
 def profile(request, username):
     watched_user = User.objects.get(username=username)
     if watched_user.profile.followers.filter(id=watched_user.id).exists():
-        watched_user.profile.followers.remove(watched_user)  # Elimina l'usuari de si mateix si és seguidor
+        watched_user.profile.followers.remove(
+            watched_user
+        )  # Elimina l'usuari de si mateix si és seguidor
 
     posts = watched_user.posts.all().order_by("-created_at")
     return render(
-        request, "profile.html", {"watched_user": watched_user, "posts": posts, 'is_following': request.user in watched_user.profile.followers.all() if request.user.is_authenticated else False}
+        request,
+        "profile.html",
+        {
+            "watched_user": watched_user,
+            "posts": posts,
+            "is_following": (
+                request.user in watched_user.profile.followers.all()
+                if request.user.is_authenticated
+                else False
+            ),
+        },
     )
+
+@login_required
+def update_profile_form(request, username):
+    if request.user.username != username:
+        return redirect("profile", username=username)
+    user = request.user
+    return render(request, "update_profile.html", {"user": user})
+
+
+@login_required
+def update_profile(request, username):
+    if request.user.username != username:
+        return redirect("profile", username=username)
+    if request.method == "POST":
+        user = request.user
+        user.profile.bio = request.POST.get("bio", "")
+        user.profile.location = request.POST.get("location", "")
+        user.profile.website = request.POST.get("website", "")
+        user.profile.save()
+    return redirect("update_profile_form", username=user.username)
+
+
+@login_required
+def update_profile_picture(request, username):
+    if request.user.username != username:
+        return redirect("profile", username=username)
+    if request.method == "POST":
+        user = request.user
+        if request.FILES.get("profile_picture"):
+            user.profile.profile_picture = request.FILES["profile_picture"]
+            user.profile.save()
+    return redirect("update_profile_form", username=user.username)
+
 
 def post(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, "post.html", {"post": post})
 
+@login_required
 def create_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -52,6 +98,7 @@ def create_post(request):
     return render(request, "home.html", {"form": form})
 
 
+@login_required
 def like_post(request, post_id):
     post = Post.objects.get(id=post_id)
     if request.user in post.likes.all():
@@ -61,6 +108,8 @@ def like_post(request, post_id):
     next = request.POST.get("next", "/")
     return redirect(next)
 
+
+@login_required
 def add_comment(request, post_id):
     post = Post.objects.get(id=post_id)
     if request.method == "POST":
@@ -69,24 +118,26 @@ def add_comment(request, post_id):
             post.comments.create(author=request.user, text=comment_text)
     return redirect("post", post_id=post_id)
 
+@login_required
 def follow_user(request, username):
     user_to_follow = User.objects.get(username=username)
     user_to_follow.profile.followers.add(request.user)
     return redirect("profile", username=username)
 
 
+@login_required
 def unfollow_user(request, username):
     user_to_unfollow = User.objects.get(username=username)
     user_to_unfollow.profile.followers.remove(request.user)
     return redirect("profile", username=username)
 
+
+@login_required
 def repost_post(request, post_id):
     original_post = Post.objects.get(id=post_id)
     if request.method == "POST":
         repost = Post.objects.create(
-            author=request.user,
-            content=original_post.content,
-            repost=original_post
+            author=request.user, content=original_post.content, repost=original_post
         )
     next = request.POST.get("next", "/")
     return redirect(next)
